f58b4fd420cda9bc0f1503365a692caf37a739eb
racheltstevens <racheltstevens@gmail.com>
2016-10-04 12:55:11 -0400

    Wrap html calls in MovieApp class.
      * class inherits from Sinatra

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,21 +1,23 @@
 require 'sinatra'
 
-get '/' do
-  File.read('index.html')
-end
-
-get 'favorites' do
-  response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
-end
-
-put '/favorites' do
-  file = JSON.parse(File.read('data.json'))
-  unless params[:name] && params[:oid]
-    return 'Invalid Request'
+class MovieApp < Sinatra::Base
+  get '/' do
+    File.read('./views/index.html')
   end
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write('data.json',JSON.pretty_generate(file))
-  movie.to_json
+
+  get '/favorites' do
+    response.header['Content-Type'] = 'application/json'
+    File.read('./data.json')
+  end
+
+  put '/favorites' do
+    file = JSON.parse(File.read('data.json')) || []
+    unless params[:name] && params[:oid]
+      return 'Invalid Request'
+    end
+    movie = { name: params[:name], oid: params[:oid] }
+    file << movie
+    File.write('data.json',JSON.pretty_generate(file))
+    movie.to_json()
+  end
 end
