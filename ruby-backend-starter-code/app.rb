dd3931fe14abc9e1216b7fc13d315581aea2219c
racheltstevens <racheltstevens@gmail.com>
2016-10-05 22:26:08 -0400

    Change to post request, returns whole favorites list

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'net/http'
+require 'json'
 
 class MovieApp < Sinatra::Base
   get '/' do
@@ -18,14 +19,14 @@ class MovieApp < Sinatra::Base
     File.read('./data.json')
   end
 
-  put '/favorites' do
-    file = JSON.parse(File.read('data.json')) || []
+  post '/favorites' do
+    file = JSON.parse(File.read('data.json'))
     unless params[:name] && params[:oid]
       return 'Invalid Request'
     end
     movie = { name: params[:name], oid: params[:oid] }
     file << movie
     File.write('data.json',JSON.pretty_generate(file))
-    movie.to_json()
+    File.read('./data.json')
   end
 end
